627dc28d51c52e3c4711cd630e73cc9c83dbbf17
PapiManu <45321589+PapiManu@users.noreply.github.com>
2022-09-01 11:00:09 +0200

    Amélioration de la date

--- script/main.js
+++ script/main.js
@@ -1,17 +1,3 @@
 console.log("😎<<--PapiScript-->>");
 
 const date = document.querySelector('.date');
-let realTime = new Date();
-
-function start() {
-    date.innerHTML = getDate(date);
-    setInterval(() => {
-        date.innerHTML = getDate(date);
-    }, 1000);
-}
-start();
-
-function getDate(date) {
-    let dateValue = realTime.getDate() + "-" + realTime.getMonth() + "-" + realTime.getFullYear() + " " + realTime.getHours() + ":" + realTime.getMinutes() + ":" + realTime.getSeconds();
-    return dateValue;
-}
